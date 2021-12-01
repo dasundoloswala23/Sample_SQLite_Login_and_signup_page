9c3ed0ddb64f0bd61c19eda09fb3787765b8b903
Tharindu Dasun <tharindudasun0@gmail.com>
2021-12-01 12:26:40 +0530

    SQLite login page

---
+++ app/src/main/java/www/playmy/loginuni/MainActivity.java
@@ -0,0 +1,118 @@
+package www.playmy.loginuni;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import www.playmy.loginuni.database.DBHelper;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText edtName,edtPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        edtName = findViewById(R.id.edtName);
+        edtPassword = findViewById(R.id.edtPassword);
+
+    }
+    public void  saveUser(View view){
+        String name = edtName.getText().toString();
+        String password = edtPassword.getText().toString();
+        DBHelper dbHelper = new DBHelper(this );
+
+        if(name.isEmpty()||password.isEmpty()){
+            Toast.makeText(this, "Enter Values ", Toast.LENGTH_SHORT).show();
+        }else {
+            dbHelper.addInfo(name,password);
+
+            long inserted = dbHelper.addInfo(name,password);
+
+            if(inserted > 0) {
+                Toast.makeText(this, "Data inserted successfully", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Something went wrong ;(", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+    }
+    public void viewAll(View view){
+        DBHelper dbHelper = new DBHelper(this);
+
+        List info = dbHelper.readAllInfo();
+        List items = new ArrayList<Integer>();
+
+        String[] data = (String[]) info.toArray(new String[0]);
+
+        //Toast.makeText(this, info.toString(), Toast.LENGTH_SHORT).show();
+
+//        Snackbar snackbar = Snackbar.make(view, info.toString(),Snackbar.LENGTH_LONG);
+//        snackbar.setAnimationMode(Snackbar.ANIMATION_MODE_SLIDE);
+//        snackbar.show();
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Users");
+        builder.setItems(data, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                String userName = data[i].split(":")[0];
+                edtName.setText(userName);
+                edtPassword.setText("*************************");
+
+            }
+        });
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+    }
+
+    public void deleteUser(View view){
+        DBHelper dbHelper = new DBHelper(this);
+
+        String userName = edtName.getText().toString();
+
+        if(userName.isEmpty()){
+            Toast.makeText(this, "Please select user to Delete", Toast.LENGTH_SHORT).show();
+        }else{
+            dbHelper.deleteInfo(userName);
+            Toast.makeText(this, "User deleted!", Toast.LENGTH_SHORT).show();
+
+            edtName.setText("");
+            edtPassword.setText("");
+        }
+
+    }
+
+    public void updateUser(View view){
+        DBHelper dbHelper = new DBHelper(this);
+        String userName = edtName.getText().toString();
+        String password = edtPassword.getText().toString();
+
+        if(userName.isEmpty()||password.isEmpty()){
+
+        }else{
+            dbHelper.updateInfo(view,userName,password);
+        }
+
+    }
+
+
+
+}
---
+++ app/src/main/java/www/playmy/loginuni/database/UsersMaster.java
@@ -0,0 +1,16 @@
+package www.playmy.loginuni.database;
+
+import android.provider.BaseColumns;
+
+public final class UsersMaster {
+
+        private UsersMaster(){}
+
+        public static class Users implements BaseColumns {
+            public static final String TABLE_NAME = "users";
+            public static final String COLUMN_NAME_USERNAME = "username";
+            public static final String COLUMN_NAME_PASSWORD = "password";
+
+        }
+    }
+
